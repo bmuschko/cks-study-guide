62e532d41ebd8cfc49580b7e08f75ff8a3070274
Benjamin Muschko <benjamin.muschko@gmail.com>
2023-02-08 17:24:16 -0700

    Pin to a specific Falco version for stability

--- app-a/ch07/falco/falco-install.sh
+++ app-a/ch07/falco/falco-install.sh
@@ -4,4 +4,4 @@ curl -s https://falco.org/repo/falcosecurity-packages.asc | sudo apt-key add -
 echo "deb https://download.falco.org/packages/deb stable main" | sudo tee -a /etc/apt/sources.list.d/falcosecurity.list
 sudo apt-get update -y
 sudo apt-get -y install linux-headers-$(uname -r)
-sudo apt-get install -y falco
+sudo apt-get install -y falco=0.33.1
